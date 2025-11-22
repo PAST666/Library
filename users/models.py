b095059f7a8a61183627f57b27f392d5026c4bb5
PAST666 <alex66651@yandex.ru>
2025-11-22 21:12:08 +0800

    Создание класса ActivationToken в модели Users

--- users/models.py
+++ users/models.py
@@ -1,12 +1,15 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 from .constants import (
     MAX_COUNTRY_LENGTH,
     MAX_EMAIL_LENGTH,
     MAX_NAME_LENGTH,
     MAX_PHONE_LENGTH,
-    # TOKEN_EXPIRES_MINUTES,
+    TOKEN_EXPIRES_MINUTES,
 )
 
 
@@ -38,3 +41,39 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class ActivationToken(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name="Пользователь",
+    )
+    token = models.UUIDField(
+        "Токен активации",
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+    )
+    created_at = models.DateTimeField(
+        "Создан",
+        auto_now_add=True,
+    )
+    expires_at = models.DateTimeField(
+        "Истекает",
+    )
+
+    def token_is_valid(self):
+        return self.expires_at > timezone.now()
+
+    def save(self, *args, **kwargs):
+        if not self.expires_at:
+            self.expires_at = timezone.now() + timezone.timedelta(
+                minutes=TOKEN_EXPIRES_MINUTES,
+            )
+
+        return super().save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return f"{self.user.username} -> {self.token}"
