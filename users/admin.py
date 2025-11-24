38a5f7e80d3175d011954ecd2ef6c8b248f08b59
PAST666 <alex66651@yandex.ru>
2025-11-24 20:45:58 +0800

    Регистрация класса User в admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "phone_number",
+        "photo",
+        "is_blocked",
+    )
+    search_fields = (
+        "first_name",
+        "last_name",
+        "email",
+        "phone_number",
+        "photo",
+        "username",
+    )
