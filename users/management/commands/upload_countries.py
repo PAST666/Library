ea2b80de1f17c0a810bc87d8b7488050d5ff67e6
PAST666 <alex66651@yandex.ru>
2025-12-04 21:37:56 +0800

    путь в команде загрузки файла в виде аргумента (add_argument), загрузка в базу с помощью bulk_create()

--- users/management/commands/upload_countries.py
+++ users/management/commands/upload_countries.py
@@ -1,4 +1,6 @@
 import json
+import os
+
 from django.core.management.base import BaseCommand
 from users.models import Country
 
@@ -6,15 +8,43 @@ from users.models import Country
 class Command(BaseCommand):
     help = 'Загрузить список стран из файла countries.json'
 
-    def handle(self, *args, **kwargs):
-        try:
-            with open('data/countries.json', 'r', encoding='utf-8') as f:
-                countries = json.load(f)
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "filepath",
+            nargs="?",
+            default="data/countries.json",
+            help="Путь до файла со странами"
+        )
 
-                for country in countries:
-                    Country.objects.create(name=country['name'], code=country['code'])
-            self.stdout.write(self.style.SUCCESS('Countries loaded successfully'))
+    def handle(self, *args, **kwargs):
+        filepath = kwargs.get("filepath")
+
+        try:
+            self.upload_countries(filepath)
+            self.stdout.write(self.style.SUCCESS("Countries loaded successfully"))
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR(f"Файл '{filepath}' не найден"))
         except json.JSONDecodeError as e:
-            self.stdout.write(self.style.ERROR(f'Ошибка декодирования JSON: {e}'))
+            self.stdout.write(self.style.ERROR(f"Ошибка декодирования JSON: {e}"))
         except Exception as e:
-            self.stdout.write(self.style.ERROR(f'Произошла ошибка: {e}'))
+            self.stdout.write(self.style.ERROR(f"Произошла ошибка: {e}"))
+
+    def upload_countries(self, filepath: str) -> None:
+        actions = {
+            "json": self.upload_countries_from_json,
+            # "csv": self.upload_countries_from_csv,
+        }
+        _, ext = os.path.splitext(filepath)
+
+        ext = ext.lower()
+        if ext not in actions:
+            raise ValueError(f"Неизвестный формат файла: {ext}")
+
+        action = actions.get(ext)
+        action(filepath)
+
+    def upload_countries_from_json(self, filepath: str) -> None:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            countries_data = json.load(f)
+        countries = [Country(name=country['name'], code=country['code']) for country in countries_data]
+        Country.objects.bulk_create(countries)
