0b11f0a2c4e61feb17c3f3cce58fc58e7c2d554e
PAST666 <alex66651@yandex.ru>
2025-12-28 21:46:04 +0800

    изменения кода в классе BookManager books/managers.py

--- books/managers.py
+++ books/managers.py
@@ -1,21 +1,13 @@
 from django.db import models
 from django.shortcuts import get_object_or_404
 
-from books.models import Book
 
-
-class BookManager(models.Model):
-    def create_book(self, user, title, genre, pages, publication_year):
-        return self.create(
-            user=user,
-            title=title,
-            genre=genre,
-            pages=pages,
-            publication_year=publication_year
-        )
+class BookManager(models.Manager):
+    def create_book(self, user, **kwargs):
+        return self.create(user=user, **kwargs)
 
     def update_book(self, book_id, new_title=None, new_pages=None, new_publication_year=None):
-        book = get_object_or_404(Book, id=book_id)
+        book = get_object_or_404(self.model, id=book_id)
         if new_title:
             book.title = new_title
         if new_pages is not None:
@@ -23,7 +15,8 @@ class BookManager(models.Model):
         if new_publication_year is not None:
             book.publication_year = new_publication_year
         book.save()
+        return book
 
     def delete_book(self, book_id):
-        book = get_object_or_404(Book, id=book_id)
+        book = get_object_or_404(self.model, id=book_id)
         book.delete()
