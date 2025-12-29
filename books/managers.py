dd3f37b855953a84aeadeb4b25f0e3ad97209d76
PAST666 <alex66651@yandex.ru>
2025-12-29 20:39:56 +0800

    def get_all_books и def search_books в классе BookManager books/managers.py

--- books/managers.py
+++ books/managers.py
@@ -20,3 +20,19 @@ class BookManager(models.Manager):
     def delete_book(self, book_id):
         book = get_object_or_404(self.model, id=book_id)
         book.delete()
+
+    def get_all_books_list(self) -> list:
+        return self.all()
+
+    def search_books(self, title=None, genre=None, publication_year=None, pages=None):
+        queryset = self.all()
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if genre:
+            queryset = queryset.filter(genre=genre)
+        if publication_year:
+            queryset = queryset.filter(publication_year=publication_year)
+        if pages:
+            queryset = queryset.filter(pages=pages)
+        return queryset
+
