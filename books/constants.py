2a16bdd9ee25485c8f351f50acb336a295fcd30a
PAST666 <alex66651@yandex.ru>
2026-01-09 19:03:31 +0800

    удаление класса Genre в constants.py

--- books/constants.py
+++ books/constants.py
@@ -5,15 +5,6 @@ MAX_NUMBER_ISBN: int = 13
 MAX_AGE_RATING_LENGTH: int = 3
 
 
-class Genre(models.TextChoices):
-    FANTASY = ("FANTASY", "Фантастика")
-    THRILLER = ("THRILLER", "Триллер")
-    HORROR = ("HORROR", "Ужас")
-    ADVENTURE = ("ADVENTURE", "Приключения")
-    FICTION = ("FICTION", "Художественная литература")
-    ROMAN = ("ROMAN", "Роман")
-
-
 class AgeRating(models.TextChoices):
     ABOVE_ZERO = ("ABOVE_ZERO", "0+")
     ABOVE_SIX = ("ABOVE_SIX", "6+")
