2b5ab0fd05f1a97e55dd77ec981305497154cace
PAST666 <alex66651@yandex.ru>
2026-01-16 21:56:48 +0800

    добавление декораторов @property, @classmethod в CRUD функции в классе Book books/models

--- books/models.py
+++ books/models.py
@@ -94,24 +94,28 @@ class Book(models.Model):
         verbose_name_plural = "Книги"
         ordering = ("title",)
 
-    def create_book(self, user, **kwargs):
-        return self.create(user=user, **kwargs)
+    @classmethod
+    def create_book(cls, user, **kwargs):
+        return cls.objects.create(user=user, **kwargs)
 
-    def update_book(self, book_id, new_title=None, new_pages=None, new_publication_year=None):
-        book = get_object_or_404(self.model, id=book_id)
+    @classmethod
+    def update_book(cls, book_id, new_title=None, new_pages=None, new_publication_date=None):
+        book = get_object_or_404(cls, id=book_id)
         if new_title:
             book.title = new_title
         if new_pages is not None:
             book.pages = new_pages
-        if new_publication_year is not None:
-            book.publication_year = new_publication_year
+        if new_publication_date is not None:
+            book.publication_date = new_publication_date
         book.save()
         return book
 
-    def delete_book(self, book_id):
-        book = get_object_or_404(self.model, id=book_id)
+    @classmethod
+    def delete_book(cls, book_id):
+        book = get_object_or_404(cls, id=book_id)
         book.delete()
 
+    @property
     def clean_isbn(self):
         if not self.isbn:
             return ""
