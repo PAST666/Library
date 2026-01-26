a61664dd8cdf3d4aade4a6600eec5c3eb1ce8f3f
PAST666 <alex66651@yandex.ru>
2026-01-26 20:54:06 +0800

    добавление def busy в books/managers.py

--- books/managers.py
+++ books/managers.py
@@ -37,3 +37,11 @@ class BookManager(models.Manager):
                 filter=Q(book_inventory__status=Status.AVAILABLE)
             )
         ).filter(available_count__gt=0)
+
+    def busy(self) -> models.QuerySet:
+        return self.annotate(
+            busy_count=Count(
+                "book_inventory",
+                filter=Q(book_inventory__status=Status.BUSY)
+            )
+        ).filter(busy_count__gt=0)
