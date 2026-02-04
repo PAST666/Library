abf247492590eddc6e001d8f79e3ddecbbf5001f
PAST666 <alex66651@yandex.ru>
2026-02-04 21:22:11 +0800

    корректировка def recent класса BookManager в books/managers.py

--- books/managers.py
+++ books/managers.py
@@ -52,6 +52,6 @@ class BookManager(models.Manager):
         return self.filter(genre=genre)
 
     def recent(self, years: int = 5) -> models.QuerySet:
-        return self.filter(publication_date__gte=datetime.now().year - years)
+        return self.filter(publication_date__year__gte=datetime.now().year - years)
 
 
