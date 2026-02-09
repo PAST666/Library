2767cd5c4bc8be250c0523137939fde772b9f88c
PAST666 <alex66651@yandex.ru>
2026-02-09 21:31:28 +0800

    удаление импорта books/models.py

--- books/models.py
+++ books/models.py
@@ -2,7 +2,6 @@ from authors.models import Author
 
 from django.db import models
 from django.core.validators import MinValueValidator
-from django.shortcuts import get_object_or_404
 from isbn_field import ISBNField
 from users.models import User
 
