162a57af9deadf804cf98b1d73d921bb93a8664d
PAST666 <alex66651@yandex.ru>
2026-02-14 17:59:03 +0800

    создание модели Author feature/authors

--- authors/models.py
+++ authors/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from .constants import MAX_NAME_LENGTH
+
+
+class Author(models.Model):
+    name: str = models.CharField(
+        "Автор",
+        max_length=MAX_NAME_LENGTH,
+        unique=True,
+        db_index=True,
+        verbose_name="Автор"
+    )
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
