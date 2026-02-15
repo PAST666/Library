57d8491605c7a3fce0132cc39d0f41ccf06e88a4
PAST666 <alex66651@yandex.ru>
2026-02-15 21:25:27 +0800

    import _ validators.py feature/authors

--- authors/validators.py
+++ authors/validators.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import RegexValidator
 
+
 class KirillicLettersValidator(RegexValidator):
     code = "invalid_name"
     message = _(
