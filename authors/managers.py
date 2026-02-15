83ea484f88894a376011f22e14faf67ec0a89543
PAST666 <alex66651@yandex.ru>
2026-02-15 14:23:56 +0800

    def by_genre класса AuthorManager в managers.py feature/authors

--- authors/managers.py
+++ authors/managers.py
@@ -4,3 +4,6 @@ from django.db import models
 class AuthorManager(models.Manager):
     def get_all_authors(self) -> models.QuerySet:
         return self.all()
+
+    def by_genre(self, genre: str) -> models.QuerySet:
+        return self.filter(genre=genre)
