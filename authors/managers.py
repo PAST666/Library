b54ccd39f6af2074b6d596bc47f1798a76facb94
PAST666 <alex66651@yandex.ru>
2026-02-18 19:54:39 +0800

    def search_authors managers.py feature/authors

--- authors/managers.py
+++ authors/managers.py
@@ -1,12 +1,26 @@
 from django.db import models
+from typing import Any
 
 
 class AuthorManager(models.Manager):
     def get_all_authors(self) -> models.QuerySet:
         return self.all()
 
-    def by_genre(self, genre: str) -> models.QuerySet:
-        return self.filter(genre=genre)
+    def search_authors(self, **kwargs) -> models.QuerySet:
+        valid_filters: dict[str, str] = {
+            "title": "title__icontains",
+            "genre": "genre",
+            "nationality": "nationality",
+        }
 
-    def book_count(self):
-        return self.book.count()
+        filters: dict[str, Any] = {}
+        for field, value in kwargs.items():
+            if valid_filters.get(field) is None:
+                continue
+            if value is not None:
+                filters[valid_filters[field]] = value
+        queryset = self.all()
+        if filters:
+            queryset = queryset.filter(**filters)
+
+        return queryset
