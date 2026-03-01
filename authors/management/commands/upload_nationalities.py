cc97533d613b55ca80e2a4dd866287f30a9c333e
PAST666 <alex66651@yandex.ru>
2026-03-01 21:51:22 +0800

    удаление избыточного кода

--- authors/management/commands/upload_nationalities.py
+++ authors/management/commands/upload_nationalities.py
@@ -2,7 +2,7 @@ import json
 import os
 
 from django.core.management.base import BaseCommand
-from authors.models import Author, Nationality
+from authors.models import Nationality
 
 
 class Command(BaseCommand):
