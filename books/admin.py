0e8573169b8b2199af38447bcd55f9b48b9e67d6
PAST666 <alex66651@yandex.ru>
2026-03-09 10:24:09 +0800

    admin.py приложения books feature/books

--- books/admin.py
+++ books/admin.py
@@ -1,1 +1,38 @@
 from django.contrib import admin
+
+from books.models import Genre, Book, BookInventory
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    ordering = ("name",)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "pages",
+        "publication_date",
+        "user",
+        "author",
+        "genre",
+        "isbn",
+        "age_rating"
+    )
+    search_fields = (
+        "title",
+        "publication_date",
+        "author",
+        "isbn",
+    )
+    ordering = ("title",)
+    list_filter = ("age_rating",)
+
+
+@admin.register(BookInventory)
+class AdminBookInventory(admin.ModelAdmin):
+    list_display = ("book", "status",)
+    search_fields = ("book",)
+    ordering = ("book",)
+    list_filter = ("status",)
