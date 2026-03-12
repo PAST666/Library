52243a38c449d68aa119fc0ff2f46e18ab9d792c
PAST666 <alex66651@yandex.ru>
2026-03-12 21:09:03 +0800

    корректировка 'Roles.ADMIN' вместо admin в def create_superuser

--- users/managers.py
+++ users/managers.py
@@ -20,6 +20,6 @@ class ActivationTokenManager(models.Manager):
 class UserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **kwargs):
         kwargs.setdefault('is_superuser', True)
-        kwargs.setdefault('role', 'admin')
+        kwargs.setdefault('role', 'Roles.ADMIN')
         birth_date = datetime.now()
         return self.create_user(email=email, password=password, birth_date=birth_date, **kwargs)
