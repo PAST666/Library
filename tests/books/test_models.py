6831894d39d838baa7ba833196751e81e21055c7
PAST666 <alex66651@yandex.ru>
2026-05-11 20:16:43 +0800

    Создание папки tests и создание первого теста - жанра с названием "фантастика"

---
+++ tests/books/test_models.py
@@ -0,0 +1,19 @@
+import pytest
+
+from books.models import Genre, Book
+
+@pytest.mark.django_db
+class TestGenreModel:
+
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.object = Genre(
+            name="Фантастика"
+        )
+        self.object.name = self.object.name.lower()
+        self.object.full_clean()
+        self.object.save()
+
+    def test_genre_created(self):
+        genre_from_db = Genre.objects.get(name="фантастика")
+        assert genre_from_db.name == "фантастика"
