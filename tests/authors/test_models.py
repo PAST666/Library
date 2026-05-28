bfd52fb541813a283aaa556613e65db937557172
PAST666 <alex66651@yandex.ru>
2026-05-28 20:27:53 +0800

    тест first_name с спецсимволами - невалидное значение

--- tests/authors/test_models.py
+++ tests/authors/test_models.py
@@ -166,3 +166,13 @@ class TestAuthorModel:
         )
         with pytest.raises(ValidationError):
             author.full_clean()
+
+    def test_first_name_field_contains_special_symbols(self):
+        expected_date = date(1990, 1, 1)
+        author = Author(
+            first_name="Иван!",
+            last_name="Иванов",
+            birth_date=expected_date,
+        )
+        with pytest.raises(ValidationError):
+            author.full_clean()
