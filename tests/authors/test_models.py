77773062053fc141c5f42af24edb5183d86b0f52
PAST666 <alex66651@yandex.ru>
2026-05-28 20:01:24 +0800

    тест first_name только с кириллицей - валидное значение

--- tests/authors/test_models.py
+++ tests/authors/test_models.py
@@ -133,3 +133,18 @@ class TestAuthorModel:
             nationality=nationality)
         with pytest.raises(ValidationError):
             author.full_clean()
+
+    def test_first_name_field_kirillic(self):
+        expected_date = date(1990, 1, 1)
+        nationality = Nationality(nationality="Русский", code="RU")
+        nationality.full_clean()
+        nationality.save()
+        author = Author(
+            first_name="Иван",
+            last_name="Иванов",
+            birth_date=expected_date,
+            nationality=nationality)
+        author.full_clean()
+        author.save()
+        author_from_db = Author.objects.get(pk=author.pk)
+        assert author_from_db.first_name == "иван"
