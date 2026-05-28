44239505f52c79ce23b923189d68cd724823d9d0
PAST666 <alex66651@yandex.ru>
2026-05-28 20:25:59 +0800

    тест first_name с цифрами - невалидное значение

--- tests/authors/test_models.py
+++ tests/authors/test_models.py
@@ -156,3 +156,13 @@ class TestAuthorModel:
         )
         with pytest.raises(ValidationError):
             author.full_clean()
+
+    def test_first_name_field_contains_numbers(self):
+        expected_date = date(1990, 1, 1)
+        author = Author(
+            first_name="Иван123",
+            last_name="Иванов",
+            birth_date=expected_date,
+        )
+        with pytest.raises(ValidationError):
+            author.full_clean()
