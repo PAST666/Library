292acb6de200d2783dc3abd52983a8cff808a68e
PAST666 <alex66651@yandex.ru>
2026-05-30 15:50:22 +0800

    тест валидация email - недопустимый домен

--- tests/users/test_models.py
+++ tests/users/test_models.py
@@ -228,3 +228,16 @@ class TestUserModel:
         user4.full_clean()
         user5.full_clean()
         user6.full_clean()
+
+    def test_email_not_valid_field(self):
+        expected_date = date(1990, 1, 1)
+        user = User(
+            username="test_user",
+            first_name="Иван",
+            last_name="Иванов",
+            email="user@protonmail.com",
+            birth_date=expected_date,
+            password="securepass123"
+        )
+        with pytest.raises(ValidationError):
+            user.full_clean()
