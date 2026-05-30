3bcd32c62dc120f8b71828b36863ad110d765b6e
PAST666 <alex66651@yandex.ru>
2026-05-30 17:52:57 +0800

    тест валидация phone_number - корректный формат +7

--- tests/users/test_models.py
+++ tests/users/test_models.py
@@ -254,3 +254,16 @@ class TestUserModel:
         )
         with pytest.raises(ValidationError):
             user.full_clean()
+
+    def test_create_user_with_phone_email_field(self):
+        expected_date = date(1990, 1, 1)
+        user = User.objects.create(
+            username="test_user",
+            first_name="Иван",
+            last_name="Иванов",
+            email="test@gmail.com",
+            birth_date=expected_date,
+            password="securepass123",
+            phone_number="+79001234567"
+        )
+        user.refresh_from_db()
