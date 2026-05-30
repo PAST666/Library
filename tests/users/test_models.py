e61b869b7c436d5ef581308dc26d374726c42a69
PAST666 <alex66651@yandex.ru>
2026-05-30 12:32:42 +0800

    тест создание записи с валидными данными

---
+++ tests/users/test_models.py
@@ -0,0 +1,17 @@
+import pytest
+from datetime import date
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+
+from users.models import Country
+
+@pytest.mark.django_db
+class TestCountryModel:
+
+    def test_create_country_with_valid_fields(self):
+        country = Country.objects.create(name="Россия", code="RU")
+        country.full_clean()
+        country.save()
+        country.refresh_from_db()
+        assert country.name == "Россия"
+        assert country.code == "RU"
