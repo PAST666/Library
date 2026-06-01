4fb506f97c592acb020f4b201ba4bd9848998e17
PAST666 <alex66651@yandex.ru>
2026-06-01 19:36:58 +0800

    def create_user, def create_superuser в users/managers.py

--- users/managers.py
+++ users/managers.py
@@ -18,8 +18,32 @@ class ActivationTokenManager(models.Manager):
 
 
 class UserManager(BaseUserManager):
+
+    def create_user(self, email, birth_date, password=None, **kwargs):
+        if not email:
+            raise ValueError("Email является обязательным полем")
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            birth_date=birth_date,
+            **kwargs
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
     def create_superuser(self, email, password=None, **kwargs):
         kwargs.setdefault('is_superuser', True)
         kwargs.setdefault('role', 'Roles.ADMIN')
-        birth_date = datetime.now()
-        return self.create_user(email=email, password=password, birth_date=birth_date, **kwargs)
+        birth_date = kwargs.pop('birth_date', datetime.now().date())
+        if isinstance(birth_date, datetime):
+            birth_date = birth_date.date()
+
+        return self.create_user(
+            email=email,
+            password=password,
+            birth_date=birth_date,
+            **kwargs
+        )
+
+
