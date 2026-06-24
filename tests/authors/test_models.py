f3167fc4eba6f823f60c4fef18067895e8673ce3
PAST666 <alex66651@yandex.ru>
2026-06-24 22:26:26 +0800

    изменение def test_create_author_instance_with_empty_last_name_field с помощью фикстуры

--- tests/authors/test_models.py
+++ tests/authors/test_models.py
@@ -105,13 +105,9 @@ class TestAuthorModel:
         with pytest.raises(ValidationError):
             author.full_clean()
 
-    def test_create_author_instance_with_empty_last_name_field(self):
-        expected_date = date(1990, 1, 1)
-        author = Author(
-            first_name="Иван",
-            last_name="",
-            birth_date=expected_date,
-        )
+    def test_create_author_instance_with_empty_last_name_field(self, author_data):
+        author = Author(**author_data)
+        author.last_name = ""
         with pytest.raises(ValidationError):
             author.full_clean()
 
